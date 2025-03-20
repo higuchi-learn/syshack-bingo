ac0f93acc3c2292cbc9a28dd2a2186a1606ae320
higuchi-learn <haruki.higuchi.000@gmail.com>
2025-03-20 23:34:21 +0900

    update: ディレクトリ構造を調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from 'next';
+import { inter } from "@/utils/fonts"
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Syshack BINGO', // ここの文字列がブラウザのタブに表示される
+  description: 'Created by plusU', // こちらはメタタグに表示される文字列
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="ja" foxified="">
+    <html lang="ja" className={`${inter.className}`}>
       <body>{children}</body>
     </html>
   );
