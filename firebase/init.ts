e6774910857c7df84d5fd34750ea5205d5c3d0a6
higuchi-learn <haruki.higuchi.000@gmail.com>
2025-03-22 22:29:24 +0900

    add: 複数のプレページ、Firestoreの紐づけ

--- firebase/init.ts
+++ firebase/init.ts
@@ -1,7 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,7 +14,5 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
-const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
-export { db, auth };
+export { db, };
